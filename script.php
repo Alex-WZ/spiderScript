1de76ca06eee700de1a900eb813439ce4ff48457
zhen wen <zhen wen>
2017-01-31 22:53:36 +0800

    remove some code

--- script.php
+++ script.php
@@ -1,16 +1,17 @@
 <?php
+/**
+ * grab answer from zhuhu.com
+ */
 include './simple_html_dom.php';
-
-
-
 $url = 'https://www.zhihu.com/node/QuestionAnswerListV2';
 
 $offset = 20;
 $finalResult = array();
+$questionId = 22592866; // you question id
+
 do{
 
-	$data['_xsrf'] = 'fab18200af3bef8575388d4d01de0d55';
-	$data['params'] = '{"url_token":45945664,"pagesize":3,"offset":' . $offset . '}';
+	$data['params'] = '{"url_token": '.$questionId.',"pagesize":3,"offset":' . $offset . '}';
 	$data['method'] = 'next';
 
 	$respJson = callApiPost($url,$data);
@@ -26,16 +27,16 @@ do{
 		$commentCount = (int)$htmlObj->find('a',9)->plaintext;
 		// var_dump($htmlObj->find('div',6)->plaintext);exit;
 		$txt = $htmlObj->find('div',7)->plaintext;
+		// save data
 		insDb($proCount,$commentId,$commentCount,$txt,$commentHtml);
 	}
 
 	unset($data,$respJson,$resp);
-
 	$offset += 10;
+
 }while (count($commentArrTmp) > 0 );
 
 echo file_put_contents("./res.txt", json_encode($finalResult));
-
 
 function callApiPost($url,$data){
 	$ch = curl_init();
@@ -50,22 +51,23 @@ function callApiPost($url,$data){
 }
 
 
+//save data
 function insDb($proCount,$commentId,$commentCount,$txt,$commentHtml){
 
-$con = mysql_connect('localhost:3306','root','vertrigo');
+	$con = mysql_connect('localhost:3306','root','vertrigo');
 
-if(!$con){
-	echo 'db err';
-	exit;
-}
-mysql_set_charset("UTF8", $con);
-mysql_select_db("test", $con);
+	if(!$con){
+		echo 'db err';
+		exit;
+	}
+	mysql_set_charset("UTF8", $con);
+	mysql_select_db("test", $con);
 
-$sql = "insert into zhihu_data (txt,proCount,commentId,commentCount,commentHtml) values ('{$txt}','{$proCount}','{$commentId}','{$commentCount}','{$commentHtml}')";
-// $sql = "insert into zhihu_data (txt,proCount,commentId,commentCount,commentHtml) values ('{txt}','{proCount}','{commentId}','{commentCount}','{commentHtml}' )";
-// file_put_contents("./text.txt", $sql);exit;
-$result = mysql_query($sql, $con);  
+	$sql = "insert into zhihu_data (txt,proCount,commentId,commentCount,commentHtml) values ('{$txt}','{$proCount}','{$commentId}','{$commentCount}','{$commentHtml}')";
+	// $sql = "insert into zhihu_data (txt,proCount,commentId,commentCount,commentHtml) values ('{txt}','{proCount}','{commentId}','{commentCount}','{commentHtml}' )";
+	// file_put_contents("./text.txt", $sql);exit;
+	$result = mysql_query($sql, $con);  
 
-echo $result;
+	echo $result;
 
 }
